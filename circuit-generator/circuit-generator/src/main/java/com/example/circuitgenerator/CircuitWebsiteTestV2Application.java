db1970e5f4b305542af40e004bbf798a51030149
FlyingWancker <harveyjackson194@gmail.com>
2024-11-30 13:08:17 -0500

    Update

---
+++ circuit-generator/circuit-generator/src/main/java/com/example/circuitgenerator/CircuitWebsiteTestV2Application.java
@@ -0,0 +1,13 @@
+package com.example.circuitgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitWebsiteTestV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitWebsiteTestV2Application.class, args);
+	}
+
+}
---
+++ circuit-generator/circuit-generator/src/test/java/com/example/circuitgenerator/CircuitWebsiteTestV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circuitgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitWebsiteTestV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
